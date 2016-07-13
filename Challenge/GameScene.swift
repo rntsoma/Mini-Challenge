11a8c30a47f60a652b2a3630d0668bd1dd5314f4
vntlab <vntlab@suportes-Mac-mini-47.local>
2016-07-13 08:54:20 -0300

    Done

--- Challenge/GameScene.swift
+++ Challenge/GameScene.swift
@@ -13,8 +13,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     let bg:SKSpriteNode=SKSpriteNode.init(imageNamed: "background")
     let hero=Hero.init(imageNamed: "spaceship")
     var enemyArray:[Enemy]=[]
-    let bulletCategory: UInt32 = 0x1 << 0
-    let enemyCategory: UInt32 = 0x1 << 0
+    let bulletCollision:UInt32 = 0x1 << 0
+    let enemyCollision:UInt32 = 0x1 << 0
     
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
@@ -30,7 +30,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         self.runAction(SKAction.repeatActionForever(sequence))
         self.addChild(bg)
         self.addChild(hero)
-//        self.physicsWorld.contactDelegate=self
+        self.physicsWorld.contactDelegate=self
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
@@ -46,10 +46,11 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             bullet.zPosition = 2
             bullet.runAction(actionSequence)
             bullet.physicsBody=SKPhysicsBody(circleOfRadius: bullet.size.width / 2)
-//            bullet.physicsBody?.usesPreciseCollisionDetection=true
-//            bullet.physicsBody?.categoryBitMask=self.bulletCategory
-//            bullet.physicsBody?.collisionBitMask=self.bulletCategory | self.enemyCategory
-//            bullet.physicsBody?.contactTestBitMask=self.bulletCategory | self.enemyCategory
+            bullet.physicsBody?.affectedByGravity=false
+            bullet.physicsBody?.categoryBitMask=self.bulletCollision
+            bullet.physicsBody?.contactTestBitMask=self.enemyCollision
+            bullet.physicsBody?.collisionBitMask=self.enemyCollision
+            bullet.physicsBody?.usesPreciseCollisionDetection=true
             self.addChild(bullet)
         }
     }
@@ -64,19 +65,25 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         let moveAction=SKAction.moveTo(CGPoint(x: 0,y: enemy.position.y), duration: 2)
         let removeAction=SKAction.removeFromParent()
         let sequence = SKAction.sequence([moveAction, removeAction])
+        enemy.physicsBody = SKPhysicsBody.init(rectangleOfSize: enemy.frame.size)
+        enemy.physicsBody?.affectedByGravity=false
+        enemy.physicsBody?.categoryBitMask=self.enemyCollision
+        enemy.physicsBody?.collisionBitMask=self.bulletCollision
+        enemy.physicsBody?.contactTestBitMask=self.bulletCollision
+        enemy.physicsBody?.usesPreciseCollisionDetection=true
         enemy.zPosition=2
         enemy.runAction(sequence)
-        enemy.physicsBody=SKPhysicsBody(circleOfRadius: enemy.size.width / 2)
-//        enemy.physicsBody?.usesPreciseCollisionDetection=true
-//        enemy.physicsBody?.categoryBitMask=self.enemyCategory
         self.enemyArray.append(enemy)
         self.addChild(enemy)
     }
     
     func didBeginContact(contact: SKPhysicsContact) {
-        
-        if(contact.bodyA.categoryBitMask == self.bulletCategory && contact.bodyB.categoryBitMask == self.enemyCategory){
-            print("Bateu, hue")
+        let firstBody=contact.bodyA.node as! SKSpriteNode
+        let secondBody=contact.bodyB.node as! SKSpriteNode
+        if(contact.bodyA.categoryBitMask == self.bulletCollision && contact.bodyB.categoryBitMask == self.enemyCollision){
+//            print("Bateu, hue")
+            firstBody.runAction(SKAction.removeFromParent())
+            secondBody.runAction(SKAction.removeFromParent())
         }
         
     }
