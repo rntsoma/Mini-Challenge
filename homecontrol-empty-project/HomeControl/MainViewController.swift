e1faa03de1597c441184f791e2422f18db468557
rntsoma <renato-ys@hotmail.com>
2016-07-13 12:00:03 -0300

    Start activities for iot

--- homecontrol-empty-project/HomeControl/MainViewController.swift
+++ homecontrol-empty-project/HomeControl/MainViewController.swift
@@ -12,21 +12,24 @@ class MainViewController:UIViewController{
     
     @IBOutlet weak var lampSwitch: UISwitch!
     @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var temperatureImage: UIImageView!
     
     override func viewDidLoad() {
-   
+        self.lampSwitch.addTarget(self, action: Selector("switchLamp"), forControlEvents: UIControlEvents.ValueChanged)
+        self.temperatureImage.userInteractionEnabled=true
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: Selector("fetchTemperature"))
+        self.temperatureImage.addGestureRecognizer(tapGestureRecognizer)
     }
     
     func fetchTemperature() {
-    
+        print("22.3 °C")
     }
     
     func switchLamp() {
-    
+        print("Changing state")
     }
     
     func fetchLampState() {
-    
     
     }
     
