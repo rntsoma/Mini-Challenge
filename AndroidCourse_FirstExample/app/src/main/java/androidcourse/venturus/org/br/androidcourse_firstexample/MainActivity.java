d445d136892bec2b409c0788d8fef655a8c54f8a
rntsoma <renato-ys@hotmail.com>
2016-07-18 11:37:17 -0300

    Add first android project

---
+++ AndroidCourse_FirstExample/app/src/main/java/androidcourse/venturus/org/br/androidcourse_firstexample/MainActivity.java
@@ -0,0 +1,25 @@
+package androidcourse.venturus.org.br.androidcourse_firstexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private int count = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button_id);
+        button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                TextView label = (TextView) findViewById(R.id.label_id);
+                count++;
+                label.setText(count + " Likes");
+            }
+        });
+    }
+}
