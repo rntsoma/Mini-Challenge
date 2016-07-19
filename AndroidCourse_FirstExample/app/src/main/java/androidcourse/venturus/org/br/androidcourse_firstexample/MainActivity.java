118a32f1ff2d6623012b3750e8228fe943f259b5
rntsoma <renato-ys@hotmail.com>
2016-07-19 11:26:25 -0300

    Add ignore, update project

--- AndroidCourse_FirstExample/app/src/main/java/androidcourse/venturus/org/br/androidcourse_firstexample/MainActivity.java
+++ AndroidCourse_FirstExample/app/src/main/java/androidcourse/venturus/org/br/androidcourse_firstexample/MainActivity.java
@@ -1,33 +1,40 @@
 package androidcourse.venturus.org.br.androidcourse_firstexample;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+
 public class MainActivity extends AppCompatActivity {
     private int count = 0;
+    private SharedPreferences myPrefs;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-/*
-        Button button = (Button) findViewById(R.id.button_id);
-        button.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                TextView label = (TextView) findViewById(R.id.label_id);
-                count++;
-                label.setText(getString(R.string.label_name, count));
-                //label.setText(count + " Likes");
-            }
-        });
-*/
+
+        myPrefs = getSharedPreferences(getString(R.string.arquivo_preferencias), Context.MODE_PRIVATE);
+        TextView label = (TextView) findViewById(R.id.label_id);
+        count = myPrefs.getInt("count", 0);
+        label.setText(getString(R.string.label_name, count));
     }
 
     public void likedAction(View view){
         TextView label = (TextView) findViewById(R.id.label_id);
         count++;
         label.setText(getString(R.string.label_name, count));
+    }
+
+    protected void onPause(){
+        super.onPause();
+        SharedPreferences.Editor editor = myPrefs.edit();
+        Log.d("debug", "count: "+ count);
+        editor.putInt("count", count);
+        editor.apply();
     }
 }
