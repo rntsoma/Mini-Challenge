ac0daf9de5ef63321f3da59bc4495273c8cd3aed
rntsoma <renato-ys@hotmail.com>
2016-07-22 11:26:03 -0300

    Implement UI, small tweaks on UI next

--- HouseControl/app/src/main/java/com/example/vntlab/housecontrol/MainActivity.java
+++ HouseControl/app/src/main/java/com/example/vntlab/housecontrol/MainActivity.java
@@ -5,9 +5,15 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        switchState();
     }
 
     @Override
@@ -24,6 +31,31 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    /*Trata acoes baseado no estado do switch*/
+    public void switchState(){
+        Switch toggle = (Switch) findViewById(R.id.mySwitch);
+        toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    ImageView myImage=(ImageView)findViewById(R.id.background_status_imagem);
+                    myImage.setImageResource(R.drawable.bgon);
+                    TextView myText=(TextView)findViewById(R.id.status_luz_texto);
+                    myText.setText(R.string.status_luz_ligada);
+                    ImageView myImage2=(ImageView)findViewById(R.id.status_lampada);
+                    myImage2.setImageResource(R.drawable.icon_luz_on);
+                }else{
+                    ImageView myImage=(ImageView)findViewById(R.id.background_status_imagem);
+                    myImage.setImageResource(R.drawable.bgoff);
+                    TextView myText=(TextView)findViewById(R.id.status_luz_texto);
+                    myText.setText(R.string.status_luz_desligada);
+                    ImageView myImage2=(ImageView)findViewById(R.id.status_lampada);
+                    myImage2.setImageResource(R.drawable.icon_luz_off);
+                }
+            }
+        });
     }
 
     @Override
